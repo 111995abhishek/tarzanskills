d51402c5a9636f9a4c52cec24a371a58c3dbf305
abhishek <111995abhishek@gmail.com>
2019-09-05 13:43:04 +0530

    day-2.py

--- day-2.py
+++ day-2.py
@@ -1,14 +1,14 @@
 print("i will count my chicken")
-print("hens",25 + 30 / 6)
-print("roosters",100 - 25 * 3 % 4)
+print("hens",25 + 30 / 6)#30
+print("roosters",100 - 25 * 3 % 4)#97
 print("now i will count my eggs:")
-print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
-print("is it true that 3 + 2 < 5 - 7?")
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)#5.75
+print("is it true that 3 + 2 < 5 - 7?")#false
 print (3 + 2 < 5 - 7)
-print("what is 3 + 2?",3 + 2)
-print("what is 5 - 7?",5 - 7)
-print("oh, that's why its false.")
+print("what is 3 + 2?",3 + 2)#5
+print("what is 5 - 7?",5 - 7)#-2
+print("oh, that's why its false.")#
 print("how about some more")
-print("is it greater?", 5 > -2)
-print("is it greater nor equal?", 5 > = 2)
-print("is it less or equal", 5 <= -2)
+print("is it greater?", 5 > -2)true
+print("is it greater nor equal?", 5 >= 2)#0gtrue
+print("is it less or equal", 5 <= -2)false
