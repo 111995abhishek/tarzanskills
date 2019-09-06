b01f35bd494c1a5d1d8a0bc5e8e4bfa714bed439
abhishek <111995abhishek@gmail.com>
2019-09-06 14:12:04 +0530

    python2.py

---
+++ python2.py
@@ -0,0 +1,10 @@
+
+first_name= str(input("enter your first name"))#taking first name from user
+last_name=input("enter your last name")#taking last name from user
+salary=int(input("enter your salary"))#taking salary from user
+age=input("enter your age")#taking age from user
+print("your name is",first_name + last_name)
+if age==int :
+print("your age is",age)
+else print(invalid input):
+print("your salary is",salary)
