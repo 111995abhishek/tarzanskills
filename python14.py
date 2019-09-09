c9c803838442f19a72238ae765904fe1b817f888
abhishek <111995abhishek@gmail.com>
2019-09-09 14:40:59 +0530

    break

---
+++ python14.py
@@ -0,0 +1,11 @@
+total_line=int(input("enter the no. of line"))
+current_line=1
+count=1
+while count<=3:
+    print('*' * current_line)
+    current_line=current_line+2
+    count=count+1
+while(3<=count<=5):
+    print('*'* current_line)
+    current_line=current_line+2
+    count
---
+++ python15.py
@@ -0,0 +1,5 @@
+while (True):
+    name=input("enter your name")
+    if name=="varun":
+        break
+        print("welcome,varun")
