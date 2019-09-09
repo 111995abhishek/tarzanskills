6f457862848cb2382e97ce6b2f0d7140cf7aaefe
abhishek <111995abhishek@gmail.com>
2019-09-09 10:51:58 +0530

    python9.py

---
+++ python9.py
@@ -0,0 +1,11 @@
+score=int(input("enter score"))
+if 25<score<=30:
+    print("grade is a")
+elif(20<score<=25):
+    print("grade is b")
+elif(15<score<=20):
+    print("grade is c")
+elif(10<score<=15):
+    print("grade is d")
+else:
+    print("grade is e or f")
