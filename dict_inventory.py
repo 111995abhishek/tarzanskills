28fda1e9281b325a0f6cee294141e1a8db304266
abhishek <111995abhishek@gmail.com>
2019-09-16 16:07:04 +0530

    day-9

---
+++ dict_inventory.py
@@ -0,0 +1,3 @@
+dict_inventory={'rope':1,'torch':6,'gold coin':42,'dagger':1,'arrow':12}
+for keys,values in dict_inventory:
+    print()
---
+++ regular_exp.py
@@ -0,0 +1,17 @@
+#s="my name is varun and my phone number is +919739921787"
+#for x in s:
+ #   if x.isdigit():
+  #      print(x)
+#s="my name is varun
+# my email id is varun.rathore@gmail.com"
+ # ""
+#import re
+#phone_number="my number is +91 9739921787"
+#razex=re.compile(r"(\+[\d]{2}+)([\d]+)")
+#matches=razex.search(phone_number)
+#print(matches.group())
+import re
+phone_number="my no is 9739921787 varun no is 1234567890 and piyush no is 9876543210"
+razex=re.compile(r"[\w\d{10}]+")
+matches=razex.findall(phone_number)
+print(*matches,sep='\n')
