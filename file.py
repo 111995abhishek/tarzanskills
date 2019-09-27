b6886aa7a0635c778fb09ecba0ccf28fd8c83930
abhishek <111995abhishek@gmail.com>
2019-09-27 17:13:14 +0530

    file-handling

---
+++ file.py
@@ -0,0 +1,115 @@
+#file=open("test.txt","r")
+#print(file.read())
+#file.close()
+
+#with open("test.txt","r") as file:
+ #   print(file.read())
+
+#with open("hello.txt","r") as content:
+#    print(content.readlines())
+
+
+#content=open("hello.txt","r")
+#for line in content:
+ #   print(line)
+#content.close()
+
+#content=open("hello.txt","r")
+#for line in content:
+ #   print(line,end='')
+#content.close()
+
+#file=open("hello.txt","r")
+#content=file.readline()
+#print(content,end='')
+#content=file.readline()
+#print(content,end='')
+#content=file.readline()
+#print(content,end='')
+#content=file.readline()
+#print(content,end='')
+#content=file.readline()
+#print(content,end='')
+#content=file.readline()
+#print(content,end='')
+#content=file.readline()
+#print(content,end='')
+#content=file.readline()
+#print(content,end='')
+#content=file.readline()
+#print(content,end='')
+#content=file.readline()
+#print(content,end='')
+#content=file.readline()
+#print(content,end='')
+
+
+#with open("hello.txt","r") as f:
+ #   for line in f:
+  #      print(line,end='')
+
+
+#with open("hello.txt","r") as f:
+#    content=f.read(10)
+ #   print(content)
+
+#with open("hello.txt","r") as f:
+ #   content=f.readline(10)
+  #  print(content)
+   # content = f.readline(10)
+    #print(content)
+    #content = f.readline(10)
+    #print(content)
+    #content = f.readline(10)
+    #print(content)
+
+
+#with open("hello.txt","r") as f:
+ #   content=f.read(5)
+  #  print(content)
+   # content = f.read(5)
+    #print(content)
+    #content = f.read(5)
+    #print(content)
+   # content = f.read(5)
+   # print(content)
+
+
+#with open("hello.txt","r") as f:
+ #   f.seek(0)
+  #  content=f.read(10)
+   # print(content)
+
+
+
+#with open("hello.txt","r") as file:
+#    size=10
+ #   content=file.read(size)
+  #  while len(content)>0:
+   #     print(content,end='**')
+    #    content=file.read(size)
+
+
+#with open("varun.txt","w") as file:
+  #  file.write("hello")
+
+#with open("varun.txt","w") as file:
+ #   file.write("test")
+  #  file.seek(0)
+   # file.write("r")
+
+
+#with open("index.jpeg","rb") as rf:
+    #with open("varun.jpeg","wb") as wf:
+        #for line in rf:
+           # wf.write(line)
+
+
+
+with open("varun.jpeg","rb") as rf:
+    with open("tarzanskills.png","wb") as wf:
+        chunk_size=4096
+        rf_chunk=rf.read(chunk_size)
+        while len(rf_chunk)>0:
+            wf.write(rf_chunk)
+            rf_chunk=rf.read(chunk_size)
